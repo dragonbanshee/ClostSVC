de050b245c4313ec39dc611d07d4da6f4d50ed2d
Kyle Smith <dragonbanshee@gmail.com>
2016-04-01 08:51:03 -0500

    Initial Commit

---
+++ CloseSVC/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace CloseSVC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] svc = Process.GetProcessesByName("client32");
+            foreach (Process process in svc)
+            {
+                Console.WriteLine("Killing process with PID " + process.Id);
+                process.Kill();
+            }
+            Console.WriteLine("Process(s) killed successfully");
+            Console.ReadLine();
+
+        }
+    }
+}
